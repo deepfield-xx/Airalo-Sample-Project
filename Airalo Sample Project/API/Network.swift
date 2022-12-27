ef90bdec252507e8f2e550b4238d5c4d7e745a31
tomwlodar <101366974+tomwlodarcz@users.noreply.github.com>
2022-12-28 00:09:37 +0100

    Add Network layer

---
+++ Airalo Sample Project/API/Network.swift
@@ -0,0 +1,64 @@
+//
+//  Network.swift
+//  Airalo Sample Project
+//
+//  Created by MyPlace on 27/12/2022.
+//
+
+import Foundation
+
+import Alamofire
+
+enum Network {
+    
+}
+
+extension Network {
+    var path: String {
+        return ""
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var encoding: ParameterEncoding {
+        return URLEncoding.default
+    }
+    
+    var parameters: Parameters? {
+        return [:]
+    }
+    
+    var headers: HTTPHeaders {
+        switch self {
+        default:
+            let headers: HTTPHeaders = [
+                "Content-Type": "application/json",
+                "Accept": "application/vnd.myplace+json; version=1.0"
+            ]
+
+            return headers
+        }
+    }
+}
+
+extension Network: URLRequestConvertible {
+    func asURLRequest() throws -> URLRequest {
+        var urlRequest = try URLRequest(url: asURL())
+        urlRequest.httpMethod = method.rawValue
+        
+        headers.forEach { header in
+            urlRequest.setValue(header.value, forHTTPHeaderField: header.name)
+        }
+        
+        return try encoding.encode(urlRequest, with: parameters)
+    }
+}
+
+extension Network: URLConvertible {
+    func asURL() throws -> URL {
+        return URL(string: "https://www.airalo.com/api/v2/")!
+            .appendingPathComponent(path)
+    }
+}
---
+++ Airalo Sample Project/Services/NetworkService.swift
@@ -0,0 +1,63 @@
+//
+//  NetworkService.swift
+//  Airalo Sample Project
+//
+//  Created by MyPlace on 27/12/2022.
+//
+
+import Foundation
+import Alamofire
+import Combine
+
+protocol NetworkServiceable {
+    func request<T: Codable>(_ endpoint: Network) -> AnyPublisher<T, APIError>
+}
+
+final class NetworkService: NetworkServiceable {
+    private let decoder = JSONDecoder()
+    
+    init() {
+//        decoder.dateDecodingStrategy = .formatted(MessagingDataFormatter())
+    }
+    
+    func request<T: Codable>(_ endpoint: Network) -> AnyPublisher<T, APIError> {
+        return AF.request(endpoint)
+            .validate()
+            .responseDecodable(of: T.self, decoder: decoder) { [unowned self] response in
+                self.printDetails(response)
+            }
+            .publishDecodable(type: T.self, decoder: decoder)
+            .flatMap { value -> AnyPublisher<T, APIError> in
+                if let type = value.value {
+                    return Just(type)
+                        .setFailureType(to: APIError.self)
+                        .eraseToAnyPublisher()
+                } else {
+                    switch value.result {
+                    case .failure(let error):
+                        break
+                    default:
+                        break
+                    }
+                    
+                    let error: APIError = self.map(value.data)
+                    return Fail(error: error).eraseToAnyPublisher()
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private func map(_ data: Data?) -> APIError {
+        if let errorBody = try? decoder.decode(APIErrorBody.self, from: data ?? Data()) {
+            return .error(errorBody)
+        } else {
+            return .unknown
+        }
+    }
+    
+    private func printDetails(_ object: Any) {
+#if DEBUG || LIVE
+        debugPrint(object)
+#endif
+    }
+}
